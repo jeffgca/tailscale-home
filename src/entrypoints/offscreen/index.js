906e13ab9d88eb4d1e621c1f35272b8ef34f735d
Jeff Griffiths <jeff@eko-recordings.ca>
2026-03-27 16:09:53 -0700

    fixed offscreen implementation

--- src/entrypoints/offscreen/index.js
+++ src/entrypoints/offscreen/index.js
@@ -1,22 +1,20 @@
 // console.log('in the offscreen page script')
 import { getIps } from '../../lib/localip';
 
-getIps()
-	.then((ips) => {
-		// console.log('IPs found in offscreen
-		browser.runtime.sendMessage({
-			type: 'LOCAL_IPS',
-			ips,
-		});
-	})
-	.catch((err) => {
-		console.error('Error getting IPs in offscreen page:', err);
-	});
+browser.runtime.onMessage.addListener((message) => {
+	console.log('message received in offscreen page', message);
 
-// browser.runtime.onMessage.addListener((message, sendResponse) => {
-// 	if (message.type === 'GET_LOCAL_IPS') {
-// 		getIps().then((ips) => {
-// 			sendResponse({ ips });
-// 		});
-// 	}
-// });
+	if (message.type === 'GET_LOCAL_IPS') {
+		getIps()
+			.then((ips) => {
+				console.log('IPs found in offscreen', ips);
+				browser.runtime.sendMessage({
+					type: 'LOCAL_IPS',
+					ips,
+				});
+			})
+			.catch((err) => {
+				console.error('Error getting IPs in offscreen page:', err);
+			});
+	}
+});
--- src/lib/offscreen.ts
+++ src/lib/offscreen.ts
@@ -1,1 +1,30 @@
-console.log('in offscreen lib');
+let creating; // A global promise to avoid concurrency issues
+
+export async function setupOffscreenDocument(path) {
+	// Check all windows controlled by the service worker to see if one
+	// of them is the offscreen document with the given path
+	const offscreenUrl = chrome.runtime.getURL(path);
+	const existingContexts = await chrome.runtime.getContexts({
+		contextTypes: ['OFFSCREEN_DOCUMENT'],
+		documentUrls: [offscreenUrl],
+	});
+
+	if (existingContexts.length > 0) {
+		return;
+	}
+
+	// create offscreen document
+	if (creating) {
+		await creating;
+		return;
+	} else {
+		creating = chrome.offscreen.createDocument({
+			url: path,
+			reasons: ['WEB_RTC'],
+			justification: 'get local IP addresses using WebRTC',
+		});
+		await creating;
+		creating = null;
+		return;
+	}
+}
